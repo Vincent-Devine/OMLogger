ee957f220537cb451116096ffbe8c43cb4195c4f
Vincent DEVINE <vincentdevine.pro@gmail.com>
2025-04-20 21:25:14 +0200

    Add level display and settings + Update log class

--- Example/VS2022/VS2022.cpp
+++ Example/VS2022/VS2022.cpp
@@ -1,12 +1,65 @@
 #include "../../OMLogger/Logger.hpp"
 
+const unsigned int PERFORMANCE_TEST_ITERATIONS = 2;
+
+double PerformanceTestv2()
+{
+	auto start = std::chrono::high_resolution_clock::now();
+
+	for (unsigned int i = 0; i < PERFORMANCE_TEST_ITERATIONS; i++)
+	{
+		OM_LOG_DEBUG("Iterations: " + std::to_string(i));
+	}
+
+	auto end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> elapsed = end - start;
+	return elapsed.count();
+}
+
 int main()
 {
-	OM::Logger::OpenFile("log.txt");
-	LOG_INFO("info message");
-	LOG_VALIDATION("validation message");
-	LOG_DEBUG("debug message");
-	LOG_WARNING("warning message");
-	LOG_ERROR("error message");
-	OM::Logger::CloseFile();
+	OM_LOG_OPEN_LOG_FILE("logfile.txt");
+	double timev2 = PerformanceTestv2();
+
+	OM_LOG_DEBUG("debug");
+	OM_LOG_INFO("info");
+	OM_LOG_WARNING("warning");
+	OM_LOG_ERROR("error");
+	OM_LOG_CRITICAL("critical");
+
+	OM_LOG_OUTUP_SETTING(OM_LOG_SETTINGS_SHOW_DATE);
+	OM_LOG_INFO("only date");
+	OM_LOG_OUTUP_SETTING(OM_LOG_SETTINGS_SHOW_DATE | OM_LOG_SETTINGS_SHOW_FILE_INFO);
+	OM_LOG_INFO("date + file info");
+	OM_LOG_OUTUP_SETTING(OM_LOG_SETTINGS_SHOW_DATE | OM_LOG_SETTINGS_SHOW_THREAD);
+	OM_LOG_INFO("date + thread");
+
+	OM_LOG_OUTUP_SETTING(OM_LOG_SETTINGS_SHOW_NONE);
+	OM_LOG_INFO("none");
+
+	OM_LOG_OUTUP_SETTING(OM_LOG_SETTINGS_SHOW_ALL);
+	OM_LOG_INFO("all");
+
+	OM_LOG_OUTPUT_LEVEL(OM_LOG_SETTINGS_LEVEL_WARNING | OM_LOG_SETTINGS_LEVEL_INFO);
+	OM_LOG_INFO("info");
+	OM_LOG_WARNING("warning");
+	OM_LOG_ERROR("error");
+	OM_LOG_CRITICAL("critical");
+	OM_LOG_OUTPUT_LEVEL(OM_LOG_SETTINGS_LEVEL_NONE);
+	OM_LOG_INFO("info");
+	OM_LOG_WARNING("warning");
+	OM_LOG_ERROR("error");
+	OM_LOG_CRITICAL("critical");
+	OM_LOG_OUTPUT_LEVEL(OM_LOG_SETTINGS_LEVEL_ALL);
+	OM_LOG_INFO("info");
+	OM_LOG_WARNING("warning");
+	OM_LOG_ERROR("error");
+	OM_LOG_CRITICAL("critical");
+
+	OM_LOG_INFO("Execution time v2 for " + std::to_string(PERFORMANCE_TEST_ITERATIONS) + " iterations: " + std::to_string(timev2) + " seconds");
+
+	OM_ASSERTION(1 + 1 == 2, "big brain");
+	// OM_ASSERTION(1 + 2 == 2, "aled les maths");
+
+	OM_LOG_CLOSE_LOG_FILE();
 }
--- OMLogger/Logger.hpp
+++ OMLogger/Logger.hpp
@@ -1,148 +1,191 @@
 #pragma once
 
-#include <filesystem>
-#include <fstream>
-#include <string>
 #include <iostream>
-#include <Windows.h>
-#include <debugapi.h>
-#include <format>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <string>
+#include <filesystem>
+#include <mutex>
+#include <thread>
 #include <chrono>
+#include <Windows.h>
 
-namespace OM
+namespace OM::Logger
 {
-    namespace Logger
-    {
-        enum class ELevel : unsigned int
-        {
-            Info,
-            Validation,
-            Debug,
-            Warning,
-            Error,
-        };
+	enum LogLevel : uint8_t
+	{
+		None		= 0,
+		Debug		= 1 << 0,
+		Info		= 1 << 1,
+		Warning		= 1 << 2,
+		Error		= 1 << 3,
+		Critical	= 1 << 4,
+		All			= Debug | Info | Warning | Error | Critical
+	};
 
-        void DebugLog(const std::string& fileName, const unsigned int line, const std::string& functionName, const std::string& logMessage, const ELevel logLevel);
-    }
+	enum LogOutputSettings : uint8_t
+	{
+		ShowNone		= 0,
+		ShowDate		= 1 << 0,
+		ShowThread		= 1 << 1,
+		ShowFileInfo	= 1 << 2,
+		ShowAll			= ShowDate | ShowThread | ShowFileInfo
+	};
+
+	class Logger
+	{
+		// Attributes
+	private:
+		static inline Logger* s_instance = nullptr;
+
+		std::mutex m_mutex;
+		std::ofstream m_logFile;
+		uint8_t m_level = LogLevel::All;
+		uint8_t m_outputSettings = LogOutputSettings::ShowAll;
+
+		// Methods
+	public:
+		static Logger* GetInstance()
+		{
+			if (!s_instance)
+				s_instance = new Logger();
+			return s_instance;
+		}
+
+		void OpenLogFile(const std::filesystem::path& path)
+		{
+			std::scoped_lock lock(m_mutex);
+			if (m_logFile.is_open())
+				m_logFile.close();
+			m_logFile.open(path, std::ios::out | std::ios::app);
+		}
+
+		void CloseLogFile()
+		{
+			if (m_logFile.is_open())
+				m_logFile.close();
+		}
+
+		void Log(uint8_t level, const char* file, int line, const char* function, const std::string& message)
+		{
+			if (!(m_level & level))
+				return;
+
+			std::ostringstream out;
+
+			if (m_outputSettings & LogOutputSettings::ShowDate)
+			{
+				auto now = std::chrono::system_clock::now();
+				auto timeTNow = std::chrono::system_clock::to_time_t(now);
+				std::tm localTime;
+				localtime_s(&localTime, &timeTNow);
+				out << '[' << std::put_time(&localTime, "%Y-%m-%d %H:%M:%S") << "] ";
+			}
+
+			if(m_outputSettings & LogOutputSettings::ShowThread)
+				out << "[Thread ID " << std::this_thread::get_id() << "] ";
+
+			if (m_outputSettings & LogOutputSettings::ShowFileInfo)
+			{
+				const char* filename = std::strrchr(file, '\\');
+				filename = filename ? filename + 1 : file;
+				out << '[' << filename << ':' << function << '@' << line << "] ";
+			}
+
+			out << LogLevelToString(level) << " ";
+
+			std::string logInfo = out.str(); // only colored part
+
+			PrintConsole(logInfo, message, level);
+			WriteFile(logInfo + message);
+		}
+
+		void SetOutputSettings(uint8_t outputSettings) { m_outputSettings = outputSettings; }
+		void SetLevel(uint8_t level) { m_level = level; }
+
+	private:
+		Logger() = default;
+		~Logger()
+		{
+			CloseLogFile();
+			delete s_instance;
+		}
+
+		void PrintConsole(const std::string& logInfo, const std::string& message, const uint8_t level)
+		{
+			static HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+			std::scoped_lock lock(m_mutex);
+
+			WORD color = 7; // default grey
+			switch (level)
+			{
+			case LogLevel::Debug:	color = 13; break;	// purple
+			case LogLevel::Info:	color = 11; break;	// cyan
+			case LogLevel::Warning: color = 14; break;	// yellow
+			case LogLevel::Error:	color = 12; break;	// red
+			case LogLevel::Critical:color = 79; break;	// white on red background
+			default: break;
+			}
+
+			SetConsoleTextAttribute(handle, color);
+			std::cout << logInfo;
+			SetConsoleTextAttribute(handle, 7);
+			std::cout << message << std::endl;
+		}
+
+		void WriteFile(const std::string& message)
+		{
+			std::scoped_lock lock(m_mutex);
+			if (m_logFile.is_open())
+				m_logFile << message << '\n';
+		}
+
+		const char* LogLevelToString(uint8_t level)
+		{
+			switch (level)
+			{
+			case LogLevel::Debug: return "[DEBUG]   ";
+			case LogLevel::Info: return "[INFO]    ";
+			case LogLevel::Warning: return "[WARNING] ";
+			case LogLevel::Error: return "[ERROR]   ";
+			case LogLevel::Critical: return "[CRITICAL]";
+			default: return "[UNKNOWN] ";
+			}
+		}
+	};
 }
 
-#define __FILENAME__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
+// Print
+#define OM_LOG_DEBUG(message) OM::Logger::Logger::GetInstance()->Log(OM::Logger::LogLevel::Debug,		__FILE__, __LINE__, __func__, message);
+#define OM_LOG_INFO(message) OM::Logger::Logger::GetInstance()->Log(OM::Logger::LogLevel::Info,			__FILE__, __LINE__, __func__, message);
+#define OM_LOG_WARNING(message) OM::Logger::Logger::GetInstance()->Log(OM::Logger::LogLevel::Warning,	__FILE__, __LINE__, __func__, message);
+#define OM_LOG_ERROR(message) OM::Logger::Logger::GetInstance()->Log(OM::Logger::LogLevel::Error,		__FILE__, __LINE__, __func__, message);
+#define OM_LOG_CRITICAL(message) OM::Logger::Logger::GetInstance()->Log(OM::Logger::LogLevel::Critical,	__FILE__, __LINE__, __func__, message);
 
-#define LOG_INFO(logMessage) OM::Logger::DebugLog(__FILENAME__, __LINE__, __func__, logMessage, OM::Logger::ELevel::Info);
-#define LOG_VALIDATION(logMessage) OM::Logger::DebugLog(__FILENAME__, __LINE__, __func__, logMessage, OM::Logger::ELevel::Validation);
-#define LOG_DEBUG(logMessage) OM::Logger::DebugLog(__FILENAME__, __LINE__, __func__, logMessage, OM::Logger::ELevel::Debug);
-#define LOG_WARNING(logMessage) OM::Logger::DebugLog(__FILENAME__, __LINE__, __func__, logMessage, OM::Logger::ELevel::Warning);
-#define LOG_ERROR(logMessage) OM::Logger::DebugLog(__FILENAME__, __LINE__, __func__, logMessage, OM::Logger::ELevel::Error);
+// Setup
+#define OM_LOG_OPEN_LOG_FILE(filePath) OM::Logger::Logger::GetInstance()->OpenLogFile(filePath);
+#define OM_LOG_CLOSE_LOG_FILE() OM::Logger::Logger::GetInstance()->CloseLogFile();
+#define OM_LOG_OUTPUT_LEVEL(level) OM::Logger::Logger::GetInstance()->SetLevel(level);
+#define OM_LOG_OUTUP_SETTING(flags) OM::Logger::Logger::GetInstance()->SetOutputSettings(flags);
 
-#ifdef DEBUG
-#define Assertion(expression, logMessage) if(!(expression)) { LOG_ERROR(logMessage); __Debugbreak(); }
+#define OM_LOG_SETTINGS_LEVEL_DEBUG		OM::Logger::LogLevel::Debug
+#define OM_LOG_SETTINGS_LEVEL_INFO		OM::Logger::LogLevel::Info
+#define OM_LOG_SETTINGS_LEVEL_WARNING	OM::Logger::LogLevel::Warning
+#define OM_LOG_SETTINGS_LEVEL_ERROR		OM::Logger::LogLevel::Error
+#define OM_LOG_SETTINGS_LEVEL_CRITICAL	OM::Logger::LogLevel::Critical
+#define OM_LOG_SETTINGS_LEVEL_NONE		OM::Logger::LogLevel::None
+#define OM_LOG_SETTINGS_LEVEL_ALL		OM::Logger::LogLevel::All
+
+#define OM_LOG_SETTINGS_SHOW_DATE		OM::Logger::LogOutputSettings::ShowDate
+#define OM_LOG_SETTINGS_SHOW_THREAD		OM::Logger::LogOutputSettings::ShowThread
+#define OM_LOG_SETTINGS_SHOW_FILE_INFO	OM::Logger::LogOutputSettings::ShowFileInfo
+#define OM_LOG_SETTINGS_SHOW_NONE		OM::Logger::LogOutputSettings::ShowNone
+#define OM_LOG_SETTINGS_SHOW_ALL		OM::Logger::LogOutputSettings::ShowAll
+
+// Assertion
+#ifdef _DEBUG
+#define OM_ASSERTION(expression, message) if(!(expression)) { OM_LOG_CRITICAL(message); __debugbreak(); }
 #else
-#define Assertion(expression, logMessage) if(!(expression)) { LOG_ERROR(logMessage); abort(); }
+#define OM_ASSERTION(expression, message) if(!(expression)) { OM_LOG_CRITICAL(message); abort(); }
 #endif
-
-namespace OM
-{
-    namespace Logger
-    {
-        static inline std::ofstream _logFile;
-
-	    static void OpenFile(const std::filesystem::path& fileName)
-	    {
-		    if (_logFile && _logFile.is_open())
-            {
-                DebugLog(__FILENAME__, __LINE__, __func__, "Log file already open.", ELevel::Warning);
-                return;
-            }
-
-            _logFile.open(fileName, std::ios::out);
-            if (!_logFile)
-            {
-                DebugLog(__FILENAME__, __LINE__, __func__, "Fail to open log file " + fileName.string() + ".", ELevel::Warning);
-                return;
-            }
-            else
-            {
-                DebugLog(__FILENAME__, __LINE__, __func__, "Open log file " + fileName.string() + ".", ELevel::Info);
-            }
-        }
-
-        static void CloseFile()
-        {
-            _logFile.close();
-            DebugLog(__FILENAME__, __LINE__, __func__, "Close log file.", ELevel::Info);
-        }
-
-        static void print(const std::string& logMessage, const ELevel& logLevel)
-        {
-            HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
-            std::string logMessageWithLogLevel;
-
-            switch (logLevel)
-            {
-            case ELevel::Info:
-            {
-                SetConsoleTextAttribute(handle, 11); // texte in blue
-                std::cout << "[Info]      ";
-                SetConsoleTextAttribute(handle, 15); // texte in white (default)
-                std::cout << logMessage << std::endl;
-                logMessageWithLogLevel = "[Info] " + logMessage + '\n';
-                break;
-            }
-
-            case ELevel::Validation:
-            {
-                SetConsoleTextAttribute(handle, 10); // texte in green
-                std::cout << "[Validation]";
-                SetConsoleTextAttribute(handle, 15); // texte in white (default)
-                std::cout << logMessage << std::endl;
-                logMessageWithLogLevel = "[Info] " + logMessage + '\n';
-                break;
-            }
-
-            case ELevel::Debug:
-            {
-                SetConsoleTextAttribute(handle, 05); // texte in green
-                std::cout << "[Debug]     ";
-                SetConsoleTextAttribute(handle, 15); // texte in white (default)
-                std::cout << logMessage << std::endl;
-                logMessageWithLogLevel = "[Debug] " + logMessage + '\n';
-                break;
-            }
-
-            case ELevel::Warning:
-                SetConsoleTextAttribute(handle, 14); // texte in yellow
-                std::cout << "[Warning]   ";
-                SetConsoleTextAttribute(handle, 15); // texte in white (default)
-                std::cout << logMessage << std::endl;
-                logMessageWithLogLevel = "[Warning] " + logMessage + '\n';
-                break;
-
-            case ELevel::Error:
-                SetConsoleTextAttribute(handle, 207); // background in red, texte in white
-                std::cout << "[Error]";
-                SetConsoleTextAttribute(handle, 15); // texte in white (default)
-                std::cout << "     " + logMessage << std::endl;
-                logMessageWithLogLevel = "[Error] " + logMessage + '\n';
-                break;
-
-            default:
-                std::cout << logMessage << std::endl;;
-                logMessageWithLogLevel = logMessage + '\n';
-                break;
-            }
-
-            if (_logFile.is_open())
-            {
-                _logFile << logMessageWithLogLevel;
-            }
-        }
-
-        inline void DebugLog(const std::string& fileName, const unsigned int line, const std::string& functionName, const std::string& logMessage, const ELevel logLevel)
-        {
-            std::string time = std::format("{:%T}", floor<std::chrono::seconds>(std::chrono::system_clock::now()));
-            std::string message = "[" + time + "]" + fileName + '(' + std::to_string(line) + "):" + functionName + ": " + logMessage;
-            OutputDebugStringA(message.c_str());
-            print(message, logLevel);
-        }
-    }
-}
